95aec0754d5f0edc2582ef1d2be5875dcc3d1053
pcooney10 <pcooney9@fordham.edu>
2017-07-18 22:21:50 -0400

    use webpack.config.js instead of .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,9 @@
 module.exports = {
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        include: /(src)/,
-        query: {
-            presets: ['react', 'es2015', 'stage-0'],
-        }
-      },
-    ],
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        include: /(node_modules\/react-native-calendars)/,
+        test: /\.jsx$/,
+        exclude: /node_modules\/(?!(react-native-calendars))/,
         use: {
           loader: 'babel-loader',
           options: {
